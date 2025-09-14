fbd39a4ae3da3bc5e74563b9ac812c31dcd01e4e
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2025-09-14 03:09:24 +0100

    feat: added input component

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   const [connected, setConnected] = useState(false);
   const [address, setAddress] = useState("");
   return (
-    <div>
+    <div className="w-[100vw] h-[100vh] ">
       {connected && (
-        <header className="flex justify-between p-4 bg-gray-700 ">
+        <header className="flex justify-between p-4 bg-gray-700 w-full">
           <img src={lumenLogo} alt="lumen-logo" className="h-8" />
           <div className="flex gap-8 ">
             <p className="text-[#33A0FF] border-b-2 border-b-[#33A0FF] font-semibold cursor-pointer ">
@@ -29,14 +29,30 @@ function App() {
           </div>
         </header>
       )}
-      <main className="flex bg-[url(./assets/lumen-background.png)] w-[100dvw] h-[100dvh]  items-center justify-center ">
-        <div className="flex flex-col justify-center items-center gap-4">
-          <img src={lumenLogo} alt="lumen logo" className="w-80" />
-          <p className="text-2xl font-bold text-white">
-            Illuminating On-Chain Intelligence
-          </p>
-          <ConnectWallet setConnected={setConnected} setAddress={setAddress} />
-        </div>
+      <main className="flex bg-[url(./assets/lumen-background.png)] h-[calc(100vh-64px)] items-center justify-center ">
+        {connected ? (
+          <div className="flex flex-col gap-4">
+            <h2 className="text-3xl text-white">Ignite a New Lumen</h2>
+            <input
+              type="text"
+              className="p-3 w-full border-1 border-[#33A0FF] rounded-[4px] bg-gray-800 h-20 outline-0 text-white"
+            />
+            <button className="bg-[#33A0FF] py-3 px-10 rounded-4xl text-white font-semibold cursor-pointer hover:bg-[#1E90FF] ">
+              Ignite
+            </button>
+          </div>
+        ) : (
+          <div className="flex flex-col justify-center items-center gap-4">
+            <img src={lumenLogo} alt="lumen logo" className="w-80" />
+            <p className="text-2xl font-bold text-white">
+              Illuminating On-Chain Intelligence
+            </p>
+            <ConnectWallet
+              setConnected={setConnected}
+              setAddress={setAddress}
+            />
+          </div>
+        )}
       </main>
     </div>
   );
